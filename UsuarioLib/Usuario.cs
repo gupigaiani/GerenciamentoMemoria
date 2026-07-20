5e5ea511c7295861b2e2767103c3e6599628d6cc
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-07-20 18:09:05 -0300

    Usando LinkedList

--- UsuarioLib/Usuario.cs
+++ UsuarioLib/Usuario.cs
@@ -10,15 +10,26 @@ public class Usuario
         Nome = nome;
         Email = email;
         Telefones = telefone;
-        ChavesDeAcesso = new List<Guid>(new Guid[10]);
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
+        // ChavesDeAcesso = new List<Guid>(new Guid[10]);
+        // Stopwatch stopwatch = new Stopwatch();
+        // stopwatch.Start();
+        // for (int i = 0; i < 1000000; i++)
+        // {
+        //     ChavesDeAcesso.Add(Guid.NewGuid());
+        // }
+        // stopwatch.Stop();
+        // Console.WriteLine($"Tempo total List em ms: {stopwatch.Elapsed.TotalMilliseconds}");
+
+        ChavesDeAcessoLinkedList = new LinkedList<Guid>();
+        Stopwatch stopwatchLinkedList = new Stopwatch();
+        ChavesDeAcessoLinkedList.AddFirst(Guid.NewGuid());
+        stopwatchLinkedList.Start();
         for (int i = 0; i < 1000000; i++)
         {
-            ChavesDeAcesso.Insert(1, Guid.NewGuid());
+            ChavesDeAcessoLinkedList.AddAfter(ChavesDeAcessoLinkedList.First, Guid.NewGuid());
         }
-        stopwatch.Stop();
-        Console.WriteLine($"Tempo total em ms: {stopwatch.Elapsed.TotalMilliseconds}");  
+        stopwatchLinkedList.Stop();
+        Console.WriteLine($"Tempo total LinkedList em ms: {stopwatchLinkedList.Elapsed.TotalMilliseconds}");  
     }
 
     public int Id { get; set; }
@@ -26,6 +37,7 @@ public class Usuario
     public string Email { get; set; }
     public List<string> Telefones { get; set; }
     public List<Guid> ChavesDeAcesso { get; set; }
+    public LinkedList<Guid> ChavesDeAcessoLinkedList { get; set; }
 
     public void PadronizaTelefones()
     {
